db130f7be59d48736bb2cff97d34769d14ad4b1a
Eden Sahlie <edensahlie19@gmail.com>
2026-05-25 14:38:14 +0300

    remove the frontend and keep the backend only

--- restaurant-frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    darkMode: "class", // must be "class" for toggling
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx}"
-    ],
-    theme: { extend: {} },
-    plugins: [],
-  };
