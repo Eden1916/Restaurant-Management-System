b3ba6ecebc404dccad36dcfcb52a7b827defb5d7
Eden Sahlie <edensahlie19@gmail.com>
2026-09-03 12:44:42 +0300

    make change in the report page of the admin to desplay the total revenue, total orders and total customers

--- restaurant-frontend/src/admin/AdminReports.jsx
+++ restaurant-frontend/src/admin/AdminReports.jsx
@@ -1,11 +1,45 @@
 import DashboardLayout from "../shared/DashboardLayout";
 import { FileText, TrendingUp, ShoppingBag, Users } from "lucide-react";
-
+import {useState, useEffect} from "react";
 export default function AdminReports() {
+
+  const [stats, setStats] = useState({revenue:0, orders:0, customers:0})
+  const token = localStorage.getItem("token");
+
+
+    useEffect(() => {
+        //fetch revenue & orders count
+      fetch(`${import.meta.env.VITE_API_URL}/orders`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+  .then((r) => r.json())
+  .then((d) => {
+    if(d.success){
+      const totalRevenue = d.orders.filter(order => order.payment_status === "completed").reduce((sum, order) => sum + (parseFloat(order.total_amount) || parseFloat(order.total_price) || 0), 0)
+      setStats((s) =>({ ...s, revenue: totalRevenue.toFixed(2), orders:d.orders.length}))
+      
+    }
+  })
+  .catch(() => {})
+
+  //fetch customer count
+  fetch(`${import.meta.env.VITE_API_URL}/admin/users`, {
+    headers: {Authorization : `Bearer ${token}`}
+  })
+  .then((r) => r.json())
+  .then((d) => {
+    if(d.success) {
+      const customers = d.users.filter(User => User.role === "customer")
+      setStats((s) => ({ ...s, user: customers.length}))
+    }
+  })
+
+    })
+  
   const reportCards = [
-    { label: "Total Revenue", value: "0 ETB", icon: TrendingUp, color: "bg-green-500" },
-    { label: "Total Orders", value: "0", icon: ShoppingBag, color: "bg-blue-500" },
-    { label: "Total Customers", value: "0", icon: Users, color: "bg-red-950" },
+    { label: "Total Revenue", value: stats.revenue, icon: TrendingUp, color: "bg-green-500" },
+    { label: "Total Orders", value: stats.orders, icon: ShoppingBag, color: "bg-blue-500" },
+    { label: "Total Customers", value: stats.user, icon: Users, color: "bg-red-950" },
   ];
 
   return (
